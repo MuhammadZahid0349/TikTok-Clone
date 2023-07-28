86e8775fcbd24ec77bdf40c17c5f3147b54a6390
MuhammadZahid0349 <zahidhashim1212@gmail.com>
2023-07-28 11:18:55 +0500

    Search and Profile Complete

--- lib/views/screens/SearchScreen.dart
+++ lib/views/screens/SearchScreen.dart
@@ -1,71 +1,74 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:tiktok_clone/views/screens/Home/profileScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/controllers/search_controller.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/models/user.dart';
+import 'package:tiktok_clone/views/screens/ProfileScreen.dart';
 
-// class SearchScreen extends StatelessWidget {
-//    SearchScreen({super.key});
+class SearchScreen extends StatelessWidget {
+  SearchScreen({super.key});
 
-//   final SearchController searchController = Get.put(SearchController());
+  SearchedController searchController = Get.put(SearchedController());
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Obx(() {
-//       return Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.red,
-//           title: TextFormField(
-//             decoration: const InputDecoration(
-//               filled: false,
-//               hintText: 'Search',
-//               hintStyle: TextStyle(
-//                 fontSize: 18,
-//                 color: Colors.white,
-//               ),
-//             ),
-//             onFieldSubmitted: (value) => searchController.searchUser(value),
-//           ),
-//         ),
-//         body: searchController.searchedUsers.isEmpty
-//             ? const Center(
-//                 child: Text(
-//                   'Search for users!',
-//                   style: TextStyle(
-//                     fontSize: 25,
-//                     color: Colors.white,
-//                     fontWeight: FontWeight.bold,
-//                   ),
-//                 ),
-//               )
-//             : ListView.builder(
-//                 itemCount: searchController.searchedUsers.length,
-//                 itemBuilder: (context, index) {
-//                   User user = searchController.searchedUsers[index];
-//                   return InkWell(
-//                     onTap: () => Navigator.of(context).push(
-//                       MaterialPageRoute(
-//                         builder: (context) => ProfileScreen(uid: user.uid),
-//                       ),
-//                     ),
-//                     child: ListTile(
-//                       leading: CircleAvatar(
-//                         backgroundImage: NetworkImage(
-//                           user.profilePhoto,
-//                         ),
-//                       ),
-//                       title: Text(
-//                         user.name,
-//                         style: const TextStyle(
-//                           fontSize: 18,
-//                           color: Colors.white,
-//                         ),
-//                       ),
-//                     ),
-//                   );
-//                 },
-//               ),
-//       );
-//     });
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: TextFormField(
+            decoration: const InputDecoration(
+              filled: false,
+              hintText: 'Search',
+              hintStyle: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            onFieldSubmitted: (value) => searchController.searchUser(value),
+          ),
+        ),
+        body: searchController.searchedUsers.isEmpty
+            ? const Center(
+                child: Text(
+                  'Search for users!',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            : ListView.builder(
+                itemCount: searchController.searchedUsers.length,
+                itemBuilder: (context, index) {
+                  UserModel user =
+                      searchController.searchedUsers[index] as UserModel;
+                  return InkWell(
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => ProfileScreen(uid: user.uid),
+                      ),
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          user.profilePhoto,
+                        ),
+                      ),
+                      title: Text(
+                        user.name,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+      );
+    });
+  }
+}
